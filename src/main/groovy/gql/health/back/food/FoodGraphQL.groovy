7c8671c52c0560935b1b5209b134b1ac816191be
Mario Garcia <mario.ggar@gmail.com>
2017-05-03 14:33:07 +0200

    Re arrange code so that it makes more sense to read

--- src/main/groovy/gql/health/back/food/FoodGraphQL.groovy
+++ src/main/groovy/gql/health/back/food/FoodGraphQL.groovy
@@ -23,31 +23,6 @@ class FoodGraphQL {
   FoodService foodService
 
   /**
-   * This type has been declared here as an instance field and not in {@link Types} as an
-   * static field because it needs to resolve a given field ('entries') using the {@link FoodService} instance.
-   *
-   * @since 0.1.0
-   */
-  GraphQLObjectType GraphQLMeal = DSL.type('Meal'){
-    description 'an entry describing what you have eaten in a given moment of the day'
-
-    addField Types.MEAL_ID
-    addField Types.MEAL_COMMENTS
-    addField Types.MEAL_DATE
-    addField Types.MEAL_TYPE
-
-    field('entries') {
-      description 'a list of different food you ate in a given meal'
-      type list(Types.GraphQLMealEntry)
-      fetcher { DataFetchingEnvironment env ->
-        def meal = env.source as Map
-
-        return foodService.findAllEntriesByMealId(meal.id as UUID)
-      }
-    }
-  }
-
-  /**
    * Defines how to get all meals of a given date
    *
    * @return an instance of {@link GraphQLFieldDefinition}
@@ -79,6 +54,31 @@ class FoodGraphQL {
       argument 'meal', Types.GraphQLMealInput
       fetcher { DataFetchingEnvironment env ->
         foodService.addNewMeal(env.arguments.meal as Map)
+      }
+    }
+  }
+
+  /**
+   * This type has been declared here as an instance field and not in {@link Types} as an
+   * static field because it needs to resolve a given field ('entries') using the {@link FoodService} instance.
+   *
+   * @since 0.1.0
+   */
+  GraphQLObjectType GraphQLMeal = DSL.type('Meal'){
+    description 'an entry describing what you have eaten in a given moment of the day'
+
+    addField Types.MEAL_ID
+    addField Types.MEAL_COMMENTS
+    addField Types.MEAL_DATE
+    addField Types.MEAL_TYPE
+
+    field('entries') {
+      description 'a list of different food you ate in a given meal'
+      type list(Types.GraphQLMealEntry)
+      fetcher { DataFetchingEnvironment env ->
+        def meal = env.source as Map
+
+        return foodService.findAllEntriesByMealId(meal.id as UUID)
       }
     }
   }
