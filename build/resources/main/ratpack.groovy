4532034829866c09f75cf01fd38577e41aa446ab
Mario Garcia <mario.ggar@gmail.com>
2017-05-03 11:54:52 +0200

    Fix build should have never been on source version control

--- build/resources/main/ratpack.groovy
+++
@@ -1,7 +0,0 @@
-import static ratpack.groovy.Groovy.ratpack
-import static gql.health.back.common.SystemResources.classpath
-
-ratpack {
-  include classpath('handlers.groovy')
-  include classpath('bindings.groovy')
-}
